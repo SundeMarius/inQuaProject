09938b913823695430c123b7e9bb85746e985919
Marius Sunde Sivertsen <marius.sunde1997@gmail.com>
2018-03-15 01:48:21 +0100

    Exercise 1 WIP

--- Ex1.py
+++ Ex1.py
@@ -1,3 +1,46 @@
-import numpy as np
+import numpy as np, lib as l
 import matplotlib.pyplot as plt
+
+#Variables and interval
+v0 = 0.2 #Initial speed of electron
+N = 2000 #Number of panels over the interval
+a = 0 # [m] left end of interval
+b = 1 # [m] right end of interval
+x = np.linspace(a,b,N)
+dx = (b-a)/N
+t = np.array([n*dx/v0 for n in range(N)])
+
+#Solve the Shrodinger equation
+H = l.Hamilton(l.V1,x)
+
+#Solution (E and psiMatrix)
+E, psiMatrix = np.linalg.eigh(H)
+
+#Develop starting state in eigen-functions; calculate coefficients c_j
+coeff = np.array([l.developCoeff(psiMatrix[j],x,v0) for j in range(N)]) #Vector with c_j for all j
+
+#Expectation of x and x**2
+def X(x): return x
+def X2(x): return x**2
+
+# EX = np.zeros(len(t))
+# EX2 = np.zeros(len(t))
+# for i in range(len(t)):
+#     EX[i] = l.expectation(X,x,coeff,psiMatrix,E,t[i])
+#     EX2[i] = l.expectation(X2,x,coeff,psiMatrix,E,t[i])
+#     print("it:",i+1," Expectation:",EX[i])
+#
+# #Calculate uncertainty delta x
+# stdX = np.sqrt(EX2-EX**2)
+# #Plot expectation of x
+# plt.plot(t,stdX)
+# plt.grid()
+# plt.show()
+
+start = l.startingState(x,v0)
+start2 = np.matmul(psiMatrix,coeff)
+print(coeff)
+for i in range(N):
+    print("x=",x[i],"\tstart1=",np.real(start[i]*np.conjugate(start[i]))
+          ,"\tstart2=",np.real(start2[i]*np.conjugate(start2[i])))
 
--- lib.py
+++ lib.py
@@ -1,81 +1,86 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-
 hbar = 1.05457e-34  # [Js] - Reduced Planck's constant
 me = 9.10938e-31  # [kg] - Electron mass
-v0 = 0.5 # [m/s] - initial speed
-intervalLength = 1.  # [m] - Length of interval
-acc = 1000  # [1] - Amount of iterations
-xvalues = np.linspace(0, intervalLength, acc)
+v0 = 0.5  # [m/s] - initial speed
 
-dx = intervalLength / acc  # [m] - Increment length
 
 def V1(x):
-	return 0
+    return 0
+
 
 def V2(x):
-	return x**2
+    return x ** 2
 
-def Hamilton(V):
-	"""
 
+def Hamilton(V, positions):
+    """
 	:param V: function(x); the potential as a function of x
-	:return: np.array (acc x acc); the matrix Hamilton operator :: Hamilton * [psi[1], ..., psi[acc]] = E * psi
+	:param positions: array of all the x-values
+	:return: np.array (N x N); the matrix Hamilton operator :: Hamilton * [psi[1], ..., psi[N]] = E * psi
 	"""
 
-	# Make matrix for V
-	v = np.zeros((acc, acc))
-	for i in range(acc):
-		v[i][i] = V(xvalues[i])
+    # Make matrix for V
+    Nt = len(positions)
+    dx = (positions[Nt - 1] - positions[0]) / (Nt - 1)
+    v = np.zeros((Nt, Nt))
+    for i in range(Nt):
+        v[i][i] = V(positions[i])
 
-	# Make the matrix itself
-	H = -2 * np.eye(acc, dtype=float) + np.eye(acc, k=1, dtype=float) + np.eye(acc, k=-1, dtype=float)
+    # Make the matrix itself
+    H = -2 * np.eye(Nt, dtype=float) + np.eye(Nt, k=1, dtype=float) + np.eye(Nt, k=-1, dtype=float)
 
-	# Scale by the right amount
-	H *= -(hbar)**2 / (2*me*(dx)**2)
+    # Scale by the right amount
+    H *= -(hbar) ** 2 / (2 * me * (dx) ** 2)
 
-	# Add potential
-	H += v
+    # Add potential
+    H += v
 
-	return H
+    return H
 
-def startingState(positions,v0):
+
+def startingState(positions, v0):
     """
     :param positions: numpy array of the positions over the interval
     :param v0: initial speed
     :return: starting state evaluated at every position over the interval (complex numpy array)
     """
-    N = len(positions)  # Number of panels over the interval
+    Nt = len(positions)  # Number of panels over the interval
+    dx = (positions[Nt - 1] - positions[0]) / (Nt - 1)
 
-    #Sigma, expectation values at t = 0
-    sigma = 10*N*dx
-    x0 = N*dx/2 #Midway on the interval
-    k0 = me*v0/hbar #speed 0.1 m/s (this is the wave-number)
-    normFac = (2*np.pi*sigma)**(-0.25)
-    gauss = np.exp(-(positions-x0)**2/(4*sigma**2))
-    planeWave = np.exp(1j*k0*positions)
+    # Sigma, expectation values at t = 0
+    sigma = 10*dx
+    x0 = (positions[0]+positions[Nt-1])/2 # Midway on the interval
+    k0 = me * v0 / hbar  # speed v0 [m/s] (this is the wave-number)
+    normFac = (2 * np.pi * sigma) ** (-0.25)
+    gauss = np.exp(-(positions - x0) ** 2 / (4 * sigma ** 2))
+    planeWave = np.exp(1j * k0 * positions)
 
-    #Return the starting state (gauss wave)
-    return np.array(normFac*gauss*planeWave)
+    # Return the starting state (gauss wave)
+    return np.array(normFac * gauss * planeWave)
 
-def developCoeff(psiJ,positions):
+
+def developCoeff(psiJ, positions, v0):
     """
     :param psiJ: array containing psi_J evaluated at every x_n
     :param positions: array of x_n
+    :param v0: inital speed
     :return: the coefficient c_j for the solution
     """
-    N = len(positions) # Number of panels over the interval
+    Nt = len(positions)  # Number of panels over the interval
+    dx = (positions[Nt-1]-positions[0])/(Nt-1)
 
-    #Check if psiJ and positions have equal lengths
-    if len(psiJ) == N:
-        #return the summation over all positions (Dot-product of the vectors)
-        return np.dot(np.conjugate(psiJ),startingState(positions,v0))*dx
+    # Check if psiJ and positions have equal lengths
+    if len(psiJ) == Nt:
+        # return the summation over all positions (Dot-product of the vectors)
+        return np.dot(np.conjugate(psiJ), startingState(positions, v0)) * dx
     else:
         print("**error** psiJ and positions have different lengths")
         return 0
 
-def expectation(X,positions,developCoeff,psiMatrix,E,t):
+
+def expectation(X, positions, developCoeff, psiMatrix, E, t):
     """
     :param X: expectation-variable as a function (define it in advance)
     :param developCoeff: numpy array of all the develop coefficients j (from j = 1 to N)
@@ -85,23 +90,24 @@ def expectation(X,positions,developCoeff,psiMatrix,E,t):
     :param t: at time t (number)
     :return: the expecetation value of X at time t
     """
-    #Define psi(x,t) at a position x_n
-    def psiXn(n,t):
+
+    # Define psi(x,t) at a position x_n
+    def psiXn(n, t):
         """
         :param n: position-index n (from 0 to N-1)
         :param t: time t
         :return:  psi(x,t) at position x at time t
         """
-        energyCoeff = np.exp(-1j * E * t / hbar)*developCoeff #numpy array of energy factors with developCoeff. for each state j
-        states = np.transpose(psiMatrix)  # Matrix with row vectors as the wave-values for each state j at position x_n
+        energyCoeff = np.exp(
+            -1j * E * t / hbar) * developCoeff  # numpy array of energy factors with developCoeff. for each state j
 
-        return np.dot(states[n],energyCoeff)
+        return np.dot(psiMatrix[n], energyCoeff)
 
-    #A psi-vector containing psi(x,t) for all positions at time t (and its conjugate)
-    N = len(positions) #Number of subintervals
-    psiVec = np.array([psiXn(n,t) for n in range(N)])
+    # A psi-vector containing psi(x,t) for all positions at time t (and its conjugate)
+    Nt = len(positions)  # Number of subintervals
+    dx = (positions[Nt - 1] - positions[0]) / (Nt - 1)
+    psiVec = np.array([psiXn(n, t) for n in range(Nt)])
     psiVecConj = np.conjugate(psiVec)
 
-    #Expectation
-    return np.dot(psiVec*X(positions),psiVecConj)*dx
-
+    # Expectation
+    return np.dot(psiVec * X(positions), psiVecConj) * dx
